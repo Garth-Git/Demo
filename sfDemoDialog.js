9717c765a41c858738c2a7a0cf173f4a4e070859
Garth-Git <78998942+Garth-Git@users.noreply.github.com>
2023-10-02 11:46:54 +0200

    Update sfDemoDialog.js

--- sfDemoDialog.js
+++ sfDemoDialog.js
@@ -4,23 +4,19 @@
     const content = document.createElement("div");
     const footer = document.createElement("footer");
     const footerBtn = document.createElement("ui5-button");
-    const footerBtn2 = document.createElement("ui5-button");
     // set attributes
-    dialog.setAttribute("header-text", "Popup");
+    dialog.setAttribute("header-text", "Third Party JavaScript");
     footer.setAttribute("slot", "footer");
     // content
-    content.innerHTML = "<h2>Sign Up</h2><p>Sign up for our newsletter</p><form>  <label for='fname'>Email Address:</label><br><input type='text' id='email' name='email' value='john.doe@demo.com'><br><br><label for='animal'>Favourite animal:</label><br><select id='animal' name='animal'><option value='rhino'>Rhino</option><option value='elephant'>Elephant</option><option value='lion'>Lion</option><option value='giraffe'>Griaffe</option></select><br><br></form>";
+    content.innerHTML = "This dialog was created by third party JS file";
     content.style.padding = "1rem";
     // footer btn
-    footerBtn.onclick = () => alert('Thank you!');
-    footerBtn.innerHTML = "Submit";
-    footerBtn2.onclick = () => dialog.close();
-    footerBtn2.innerHTML = "Close";
+    footerBtn.onclick = () => dialog.close();
+    footerBtn.innerHTML = "Close";
     // append elements
     dialog.appendChild(content);
     dialog.appendChild(footer);
     footer.appendChild(footerBtn);
-    footer.appendChild(footerBtn2);
     document.body.appendChild(dialog);
     // open dialog
     dialog.show();
